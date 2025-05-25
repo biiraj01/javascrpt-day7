4d739d69a0bc7df9074f5dad8f724099ed82447b
biiraj01 <biraj2003pujari@gmail.com>
2025-05-25 15:49:55 +0545

    mes

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+let DATA = "WEBSITE DATA";
+
 class User {
     constructor(name,email){
         this.name = name;
         this.email = email;
     }
     viewData(){
-        console.log("Name :", this.name);
-        console.log("Email :", this.email);
+        console.log("Data: " , DATA);
+    }
+}
+
+class Admin extends User {
+    constructor(name , email){
+        super(name,email)
+
+    }
+    editData(name, email){
+      DATA = "New Data";
     }
 }
 
 let biraj = new User("Biraj","biraj2003pujari@gmail.com");
+
+let obj = new Admin("Umika", "umika@gmail.com");
